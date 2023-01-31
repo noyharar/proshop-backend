74d0bab61b0bce0f11389da4c7aad679abe7665a
noyharar <noyharar@post.bgu.ac.il>
2023-01-31 17:43:17 +0200

    GET top rated products

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,10 @@
 import express from "express";
 var router = express.Router();
-import {getProducts,getProductById,deleteProduct,createProduct,updateProduct,createProductReview} from "../controllers/productController.js";
+import {getProducts,getProductById,deleteProduct,createProduct,updateProduct,createProductReview,getTopProducts} from "../controllers/productController.js";
 import {protect, admin} from '../Middlewares/authMiddleware.js'
 
 
+router.route('/top').get(getTopProducts)
 router.route('/').get(getProducts)
     .post(protect,admin,createProduct);
 router.route('/:id/review').post(protect,createProductReview)
